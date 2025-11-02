9fa43a51e8134eece9a68ae2c05a037b2b669829
hugoanteh <110794474+hugoanteh@users.noreply.github.com>
2025-11-02 15:20:47 -0500

    Update Prueba.py

--- Prueba.py
+++ Prueba.py
@@ -1,10 +1,33 @@
 from dotenv import load_dotenv
+from openai import OpenAI #OpenAI Library
 import discord, os
 
 load_dotenv()
+OPENAI_KEY=os.getenv('OPENAI_KEY')
+oa_client=OpenAI(api_key=OPENAI_KEY)
+
+#Ask openai - respond like a interviewer
+
+def call_openai(question):
+    #call the openai api
+    completion=oa_client.chat.completions.create(
+        model="gpt-4o"
+        messages=[
+            {
+                "role"="user",
+                "content": f"Respond like a interviewer to the following question: {question}",
+            }
+        ]
+    )
+
+    #Print the response
+    response = completion.choices[0].message.content
+    print(response)
+    return response
+
+#Set up the intents
 intents = discord.Intents.default()
 intents.message_content = True
-
 client = discord.Client(intents=intents)
 
 @client.event
@@ -17,6 +40,14 @@ async def on_message(message):
         return
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
+    if message.content.startswith('$question'):
+        print(f"Message: {message.content}")
+        message_content = message.content.split("question")[1]
+        print(f"Question: {message.content}")
+        response = call_openai(message_content)
+        print(f"Assistant: {response}")
+        print("---")
+        await message.chanel.send(response)
 
 token = os.getenv('TOKEN')
 if not token:
